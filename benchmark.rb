54887dbe71237e5ce62da1405227e942fc516895
rlm33 <maeseraitokun@gmail.com>
2012-11-15 20:43:20 +0100

    benchmark con log_benchmark.txt para basura y funcion para replicar con otros dominios

--- benchmark.rb
+++ benchmark.rb
@@ -1,19 +1,22 @@
 require 'benchmark'
 
 $decrypter = "./dist/Debug/GNU-Linux-x86/ic_decrypt"
-$downcase_size = 26
 
 $to_test = "alpha gamma light zoned"
 
-def ic_decrypt args
-	system("#{$decrypter} #{args}")
+def ic_decrypt to_test, domain, numthreads=nil
+	system("#{$decrypter} #{to_test} -d #{domain} #{"-p -n #{numthreads}" if numthreads} > log_benchmark.txt")
 end
 
-Benchmark.bm do |x| 
-	puts 'DOWNCASE:'
-	x.report("Simple:\n") { ic_decrypt($to_test) }
-	x.report("2 threads:\n") { ic_decrypt("#{$to_test} -p -n 2") }
-	x.report("3 threads:\n") { ic_decrypt("#{$to_test} -p -n 3") }
-	x.report("4 threads:\n") { ic_decrypt("#{$to_test} -p -n 4") }
-	x.report("#{$downcase_size} threads\n:") { ic_decrypt("#{$to_test} -p -n #{$downcase_size}") }
+def benchmark to_test, domain_name, domain_size
+	Benchmark.bm do |x| 
+		puts domain_name
+		x.report("Simple:\n") { ic_decrypt(to_test, domain_name) }
+		x.report("2 threads:\n") { ic_decrypt(to_test, domain_name, 2) }
+		x.report("3 threads:\n") { ic_decrypt(to_test, domain_name, 3) }
+		x.report("4 threads:\n") { ic_decrypt(to_test, domain_name, 4) }
+		x.report("#{$downcase_size} threads\n:") { ic_decrypt(to_test, domain_name, domain_size) }
+	end
 end
+
+benchmark $to_test, 'DOWNCASE', 26
