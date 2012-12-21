fb1075ad102880ff07a3fd04234808f0174dbb1e
rlm33 <maeseraitokun@gmail.com>
2012-12-21 03:18:00 +0100

    benchmark mpi

--- benchmark.rb
+++ benchmark.rb
@@ -1,6 +1,6 @@
 require 'benchmark'
 
-$decrypter = "./dist/Debug/GNU-Linux-x86/ic_decrypt"
+$decrypter = "./decrypter"
 
 $to_test = %w{alpha gamma light pulse zoned}
 
@@ -8,14 +8,15 @@ def ic_decrypt to_test, domain, numthreads=nil
 	system("#{$decrypter} #{to_test} -d #{domain} #{"-p -n #{numthreads}" if numthreads} > log_benchmark.txt")
 end
 
-def benchmark to_test, domain_name, domain_size
+def benchmark to_test, domain_name = 'DOWNCASE', domain_size = 26
 	puts "#{domain_name} for #{to_test}"
 	Benchmark.bm do |x| 
-		x.report("Simple:") { ic_decrypt(to_test, domain_name) }
-		x.report("2 threads:") { ic_decrypt(to_test, domain_name, 2) }
-		x.report("3 threads:") { ic_decrypt(to_test, domain_name, 3) }
-		x.report("4 threads:") { ic_decrypt(to_test, domain_name, 4) }
-		x.report("#{domain_size} threads:") { ic_decrypt(to_test, domain_name, domain_size) }
+		#x.report("Simple:") { ic_decrypt(to_test, domain_name) }
+		#x.report("2 threads:") { ic_decrypt(to_test, domain_name, 2) }
+		#x.report("3 threads:") { ic_decrypt(to_test, domain_name, 3) }
+		#x.report("4 threads:") { ic_decrypt(to_test, domain_name, 4) }
+		#x.report("#{domain_size} threads:") { ic_decrypt(to_test, domain_name, domain_size) }
+		x.report("MPI:") { ic_decrypt(to_test+" -mpi", domain_name) }
 	end
 end
 
